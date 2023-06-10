f6b3863aaf5cf0b596618dd267c59a6c22b2ed51
Christofmeg <4357405+Christofmeg@users.noreply.github.com>
2023-06-10 08:16:11 +0200

    Forge 1.18.x - Network packets
    Added packets that gets sent from client to server to check if client is pressing CTRL.
    Fixes #8 with Forge for now

--- Forge/src/main/java/com/christofmeg/fastentitytransfer/platform/ForgePlatformHelper.java
+++ Forge/src/main/java/com/christofmeg/fastentitytransfer/platform/ForgePlatformHelper.java
@@ -4,10 +4,14 @@ import com.christofmeg.fastentitytransfer.platform.services.IPlatformHelper;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.loading.FMLLoader;
 
+/**
+ * The ForgePlatformHelper class provides platform-specific utility methods for Forge.
+ * It implements the IPlatformHelper interface.
+ */
 public class ForgePlatformHelper implements IPlatformHelper {
 
     /**
-     * Gets the name of the current platform
+     * Gets the name of the current platform.
      * @return The name of the current platform.
      */
     @Override
@@ -16,8 +20,8 @@ public class ForgePlatformHelper implements IPlatformHelper {
     }
 
     /**
-     * Checks if a mod with the given id is loaded.
-     * @param modId The mod to check if it is loaded.
+     * Checks if a mod with the given ID is loaded.
+     * @param modId The ID of the mod to check if it is loaded.
      * @return True if the mod is loaded, false otherwise.
      */
     @Override
@@ -26,8 +30,8 @@ public class ForgePlatformHelper implements IPlatformHelper {
     }
 
     /**
-     * Check if the game is currently in a development environment.
-     * @return True if in a development environment, false otherwise.
+     * Checks if the game is currently in a development environment.
+     * @return True if the game is in a development environment, false otherwise.
      */
     @Override
     public boolean isDevelopmentEnvironment() {
