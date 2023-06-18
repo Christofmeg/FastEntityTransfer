7097f8de31f1c6c222805d785e330e7c4737ad4f
Christofmeg <4357405+Christofmeg@users.noreply.github.com>
2023-06-18 11:53:55 +0200

    Update CommonConstants.java

--- Common/src/main/java/com/christofmeg/fastentitytransfer/CommonConstants.java
+++ Common/src/main/java/com/christofmeg/fastentitytransfer/CommonConstants.java
@@ -1,11 +1,6 @@
 package com.christofmeg.fastentitytransfer;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class CommonConstants {
 
 	public static final String MOD_ID = "fastentitytransfer";
-	public static final String MOD_NAME = "Fast Entity Transfer";
-	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
 }
