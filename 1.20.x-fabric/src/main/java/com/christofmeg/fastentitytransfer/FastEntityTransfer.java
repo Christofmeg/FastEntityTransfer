23a5eaa1d363f47be9964da3e296923f13c564ec
Christofmeg <4357405+Christofmeg@users.noreply.github.com>
2024-01-31 06:33:30 +0100

    wip 1.20 1 branch

---
+++ 1.20.x-fabric/src/main/java/com/christofmeg/fastentitytransfer/FastEntityTransfer.java
@@ -0,0 +1,45 @@
+package com.christofmeg.fastentitytransfer;
+
+import com.christofmeg.fastentitytransfer.network.PacketHandler;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.InteractionResult;
+
+public class FastEntityTransfer implements ModInitializer {
+
+    public static final ResourceLocation CTRL_KEY_PACKET_ID = new ResourceLocation(CommonConstants.MOD_ID, "ctrl_key_is_down");
+
+    public static boolean isSprintKeyDown = false;
+
+    @Override
+    public void onInitialize() {
+        PacketHandler.registerPackets();
+        CommonClickInteractions.init();
+
+        AttackBlockCallback.EVENT.register((player, level, hand, pos, direction) -> {
+            if(isSprintKeyDown) {
+                InteractionResult result = CommonClickInteractions.onLeftClickBlock(player, level, hand, pos, direction, true);
+                if (result == InteractionResult.CONSUME) {
+                    isSprintKeyDown = false;
+                    return result;
+                }
+            }
+            return InteractionResult.PASS;
+        });
+
+        UseBlockCallback.EVENT.register((player, level, hand, blockHitResult) -> {
+            if(isSprintKeyDown) {
+                InteractionResult result = CommonClickInteractions.onRightClickBlock(player, level, hand, blockHitResult, true);
+                if (result == InteractionResult.CONSUME) {
+                    isSprintKeyDown = false;
+                    return result;
+                }
+            }
+            return InteractionResult.PASS;
+        });
+
+    }
+
+}
---
+++ 1.20.x-forge/src/main/java/com/christofmeg/fastentitytransfer/ClientEvents.java
@@ -0,0 +1,42 @@
+package com.christofmeg.fastentitytransfer;
+
+import com.christofmeg.fastentitytransfer.network.PacketHandler;
+import com.christofmeg.fastentitytransfer.network.SprintKeyPacket;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * The ClientEvents class handles client-side events related to left and right click interactions.
+ * It subscribes to Forge's event bus for handling these events on the client-side.
+ */
+@Mod.EventBusSubscriber(modid = CommonConstants.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+public class ClientEvents {
+
+    /**
+     * Event handler for the LeftClickBlock event on the client-side.
+     * Sends a sprint key packet to the server if the sprint key is pressed.
+     * @param event The LeftClickBlock event.
+     */
+    @OnlyIn(Dist.CLIENT)
+    @SubscribeEvent
+    public static void onLeftClickBlock(PlayerInteractEvent.LeftClickBlock event) {
+        boolean isCtrlKeyDown = Minecraft.getInstance().options.keySprint.isDown();
+        PacketHandler.CHANNEL.sendToServer(new SprintKeyPacket(isCtrlKeyDown));
+    }
+
+    /**
+     * Event handler for the RightClickBlock event on the client-side.
+     * Sends a sprint key packet to the server if the sprint key is pressed.
+     * @param event The RightClickBlock event.
+     */
+    @OnlyIn(Dist.CLIENT)
+    @SubscribeEvent
+    public static void onRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
+        boolean isCtrlKeyDown = Minecraft.getInstance().options.keySprint.isDown();
+        PacketHandler.CHANNEL.sendToServer(new SprintKeyPacket(isCtrlKeyDown));
+    }
+}
